4db8e21f0e14b3cbd0e2a7fc8175ba45525e6688
Lucy <solouser019@gmail.com>
2026-01-26 15:07:41 +0530

    completed the js drum kit assignement

--- Day1_BasicHtmlCss/24Jan(DOM)/Game2/script.js
+++ Day1_BasicHtmlCss/24Jan(DOM)/Game2/script.js
@@ -1,15 +1,27 @@
+window.addEventListener("keydown", (e) => {
+  if (e.repeat) return; // prevents buzzing on long press
 
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio || !key) return;
 
-let h1 = document.querySelector('h1');
-h1.addEventListener('click', () => {
-    console.log('Hello');
-},{
-    once: true  // it use to run only once
+  key.classList.add("playing");
+
+  // Play the audio from start
+  audio.currentTime = 0;
+  audio.play();
+
+  // Stop the audio after 2 seconds (2000 ms)
+  const duration = key.dataset.duration || 2000;
+
+  setTimeout(() => {
+    audio.pause();
+    audio.currentTime = 0;
+  }, 500);
 });
 
-let main = document.querySelector('main');
-main.addEventListener('click', () => {
-    console.log('main');
-},{
-    capture: true // it is used to go from parent to children
+window.addEventListener("keyup", (e) => {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!key) return;
+  key.classList.remove("playing");
 });
