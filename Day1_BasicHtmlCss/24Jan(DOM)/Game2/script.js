a350bc8a199048593eefdba326b47a2a4d552308
Lucy <solouser019@gmail.com>
2026-01-26 15:24:48 +0530

    add error feature inside the js drum kit

--- Day1_BasicHtmlCss/24Jan(DOM)/Game2/script.js
+++ Day1_BasicHtmlCss/24Jan(DOM)/Game2/script.js
@@ -1,9 +1,22 @@
+const errorMessage = document.getElementById("error-message");
 window.addEventListener("keydown", (e) => {
   if (e.repeat) return; // prevents buzzing on long press
 
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio || !key) return;
+  
+  if (!audio || !key) {
+    errorMessage.textContent = `🚫 Key "${e.key}" is not mapped! 🚫`;
+    errorMessage.style.display = "block";
+ 
+
+    setTimeout(() => {
+      errorMessage.style.display = "none";
+    }, 1000); // hide after 2 seconds
+
+    return;
+  }
+
 
   key.classList.add("playing");
 
