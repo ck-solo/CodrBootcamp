90c1f55c530a915c1fd9640a5bf8278e7fe98d9c
Lucy <solouser019@gmail.com>
2026-01-28 23:19:58 +0530

    submitting error

---
+++ Day1_BasicHtmlCss/28Jan(Storage)/script.js
@@ -0,0 +1,25 @@
+let main = document.querySelector("main");
+let container = document.querySelector(".container");
+let card = document.querySelector(".card");
+
+let form = document.querySelector(".form");
+let formForm = document.querySelector(".form form");
+let formInput = document.querySelectorAll(".form input");
+let formButton = document.querySelector(".form button");
+
+formForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+//   let name = formInput[0].value;
+//   let email = formInput[1].value;
+//   let number = formInput[2].value;
+//   let image = formInput[3].value;
+
+//   localStorage.setItem("name", name);
+//   localStorage.setItem("email", email);
+//   localStorage.setItem("number", number);
+//   localStorage.setItem("image", image);
+ 
+//   console.log(localStorage);
+
+console.log("Hello");
+}); 
