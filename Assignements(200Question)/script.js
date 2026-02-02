2a3a03947a19c436b299cff37e012dd2d26bef3f
Lucy <solouser019@gmail.com>
2026-02-02 11:34:00 +0530

    Solve 25 to 36 question of 200 question

--- Assignements(200Question)/script.js
+++ Assignements(200Question)/script.js
@@ -1,7 +1,6 @@
 // Part1. Basic Console Usage (10 Questions)
 // {
 
-    
 //     //	1.	Log your name and favorite hobby to the console.
 // // ans
 // // console.log("Lucy")
@@ -26,7 +25,6 @@
 // // count = 5;                 // update value
 // // console.log(count);        // after update
 
-
 // // 6.	Use console.error() to simulate an error message.
 // // console.error("this is error")
 
@@ -41,11 +39,9 @@
 // // const age = 21;
 // // console.log(age > 18)
 
-
 // // 10.	Log the result of 100 / 0 and observe the output.
 // // console.log(100/0) // output is infinity
 // }
-
 
 // Part2. Variables and Data Types (10 Questions)
 // {
@@ -83,7 +79,7 @@
 
 // 	// 18.	Declare a variable without assigning a value. Log its type.
 //     // let declare
-//     // console.log(typeof declare)    
+//     // console.log(typeof declare)
 
 // 	// 19.	Create a variable with undefined and log its type.
 //     // let letter = undefined
@@ -96,40 +92,107 @@
 // }
 
 // Part3. Loop
-	// 21.	Write a for loop to print numbers from 1 to 50.
-    // for (let i = 0; i < 51; i++) {
-    //     console.log(i)
-        
-    // }
-   
-	// 22.	Use a while loop to sum the numbers from 1 to 10.
-    // let sum = 0;
-    // let i = 1;
-    // while (i <=10) {
-    //     sum =sum + i;
-    //     i++        
-    // }
-    // console.log(sum)
+// {
+// // 21.	Write a for loop to print numbers from 1 to 50.
+// // for (let i = 0; i < 51; i++) {
+// //     console.log(i)
 
-	// 23.	Create a for...of loop to log each character of the string "JavaScript".
-    // let name = "JavaScript";
-    // for(let char of name){
-    //     console.log(char)
-    // }
+// // }
 
-	// 24.	Write a for loop that skips even numbers between 1 and 20.
-    // let i = 0
-    // for (let i = 0; i <=10 ; i++) {
-    //     if(i%2 == 0){
-    //         console.log(i)
-    //     }
-    // } 
+// // 22.	Use a while loop to sum the numbers from 1 to 10.
+// // let sum = 0;
+// // let i = 1;
+// // while (i <=10) {
+// //     sum =sum + i;
+// //     i++
+// // }
+// // console.log(sum)
+
+// // 23.	Create a for...of loop to log each character of the string "JavaScript".
+// // let name = "JavaScript";
+// // for(let char of name){
+// //     console.log(char)
+// // }
+
+// // 24.	Write a for loop that skips even numbers between 1 and 20.
+// // let i = 0
+// // for (let i = 0; i <=10 ; i++) {
+// //     if(i%2 == 0){
+// //         console.log(i)
+// //     }
+// // }
+
+// // 25.	Use a do...while loop to log numbers from 5 to 1.
+// // let i = 10;
+
+// // do {
+// //   console.log(i);
+// //   i--;
+// // } while (i >= 1);
+
+// // 26.	Create a for loop that calculates the factorial of 5.
+// // let fact = 1
+// // for (let i = 5; i > 0; i--) {
+// //     fact = fact * i;
+// //      console.log("Step with i =", i, "=> factorial =", fact);
+
+// // }
+// // console.log(fact)
+
+// // 27.	Write a nested loop to print a 3x3 grid of numbers.
+
+// // 28.	Use a for loop to reverse an array [1, 2, 3, 4].
+
+// // for (let i = 4; i > 0; i--) {
+// //     console.log(i)
+// // }
+
+// // 29.	Write a while loop that logs numbers from 1 to 100 divisible by 5.
+// // let i = 1;
+// // while (i <= 100) {
+// // if(i% 5 ==0){
+// //   console.log(i)
+// // }
+// // i++;
+// // }
+
+// // 30.	Use a for...in loop to iterate over an object and log its keys.
+// // let data = {
+// //   name: "krishna",
+// //   age: 23,
+// //   gender: "Male",
+// // };
+
+// // for (const key in data) {
+// //   console.log(key);
+// // }
+// }
+
+
+// Part4. Arrays
+	// 31.	Create an array of your top 5 favorite movies and log it.
+  let dataa = [1,2,3,4,"Hero","Villain"]
+  // console.log(dataa)
+
+	// 32.	Find and log the second element of an array.
+  // console.log(dataa[1]);
+
+	// 33.	Add two new elements to the start of an array using .unshift().
+  // dataa.unshift("marvel","IronMan")
+  // console.log(dataa)
+
+	// 34.	Remove the last element of an array and log the updated array.
+  // dataa.pop()
+  // console.log(dataa)
+
+	// 35.	Use .slice() to extract the first three elements of an array.
+  // var dats = [1,2,3,4,"Hero","Villain"] 
+  
+  // console.log(dats.slice(1, 4))
+
+	// 36.	Find the index of a specific element in an array using .indexOf().
+  
 
 
 
-
-
-
-
-
-
+  
