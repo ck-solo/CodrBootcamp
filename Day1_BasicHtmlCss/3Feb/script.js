e05bccc3ba7cf94b622047a78edf118775e181a8
Lucy <solouser019@gmail.com>
2026-02-03 13:43:21 +0530

    OOPS Concept completed

---
+++ Day1_BasicHtmlCss/3Feb/script.js
@@ -0,0 +1,61 @@
+ 
+// class  classroom {
+
+//     constructor(students, classes,subjects){
+//         this.students = students;
+//         this.classes = classes
+//         this.subjects = subjects
+//     }
+//     print(){
+//         console.log(this.classes)
+// }
+// }
+// let school = new classroom(30,12,"Maths")
+// let school2 = new classroom(30,11,"science")
+// let school3 = new classroom(30,2,"English")
+
+// // console.log(school)
+// // console.log(school2)
+// // console.log(school3)
+
+
+// class Human{
+//     constructor(name,language){
+//         this.name = name; 
+//         this.language = language
+//     }
+//     naam(){
+//         console.log(`${this.name}, naaam `)
+//     }
+    
+//     bhasha(){
+//         console.log(`${this.language}, language  `)
+//     }
+
+// }
+
+// class indian extends Human{
+//     naam(){
+//         console.log(`${this.name} `)
+//     }
+//     bhasha(){
+//         console.log(`${this.language} `)
+//     }
+
+//     // bhasha(){
+//     //     console.log(`${this.language} `)
+//     // }
+
+// }
+
+
+// let northindian = new indian("rohan","english")
+// northindian.naam()
+// northindian.bhasha()
+// // console.log(northindian)
+
+
+
+((a,b,c) =>{
+    console.log(a + b + c);
+})(1,2,3)
