78c5bc823f4c4252063cbc0a5ddc295a7c459682
Lucy <solouser019@gmail.com>
2026-03-17 15:18:28 +0530

    class 11 : learn about socket.io and build the real time chat application project

---
+++ Backend/Day11-17-March/class/frontend/src/services/app.socket.js
@@ -0,0 +1,16 @@
+import {io} from "socket.io-client"
+
+let socket ;
+
+export const connectSocket = () => {
+    socket = io("http://localhost:3000")
+}
+
+export const addListener = (event,callback) => {
+
+    socket.on(event,callback)
+}
+
+export const emitEvent = (event,data) => {
+    socket.emit(event,data)
+}
