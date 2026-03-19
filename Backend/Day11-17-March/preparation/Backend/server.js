71c8bb9016e347d618fed0e7c2628d8ea6bd9164
Lucy <solouser019@gmail.com>
2026-03-19 10:51:32 +0530

    socket io revisoin of day11 prepartion part

---
+++ Backend/Day11-17-March/preparation/Backend/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+
+const app = express();
+
+app.get("/",(req,res)=>{
+  res.status(200).json({message:"Hello world"})
+})
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, { 
+    cors:{
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+ });
+
+io.on("connection", (socket) => {
+    console.log(" A user is connected");
+  socket.on("disconnect", () => {
+    console.log("A user is disconnected")
+  })
+
+  socket.on("hero", msg =>{
+    socket.broadcast.emit("message", msg)
+  })
+});
+
+httpServer.listen(3000,()=>{
+    console.log("server is running on port 3000")
+});
