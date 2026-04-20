723b0804e46533a8b964c0d62df765a4b1307e9d
Lucy <solouser019@gmail.com>
2026-04-20 14:33:30 +0530

    feat: Implement user authentication and authorization
    - Add auth.controller.js for user registration, login, and profile retrieval
    - Create chat.controller.js for managing chat functionalities
    - Implement post.controller.js for creating, fetching, liking, and commenting on posts
    - Add story.controller.js for creating and fetching user stories
    - Introduce user.controller.js for user profile management and follow functionalities
    - Implement middleware for authentication (auth.middleware.js) and request validation (validate.middleware.js)
    - Create models for chat messages (Chat.model.js), follows (follow.model.js), posts (post.model.js), stories (story.model.js), and users (user.model.js)
    - Set up routes for authentication (auth.route.js), chat (chat.route.js), posts (post.route.js), stories (story.route.js), and user management (user.routes.js)
    - Implement file upload service (api.service.js) for handling media uploads
    - Set up socket.io for real-time messaging in app.socket.js
    - Add validation schemas for user registration, login, and follow requests
    - Configure docker-compose for backend service setup

---
+++ Backend/Docker/Day3-Dkr-20April/Class/Backend/src/controller/user.controller.js
@@ -0,0 +1,299 @@
+import userModel from "../models/user.model.js";
+import followModel from "../models/follow.model.js";
+import mongoose from "mongoose";
+import postModel from "../models/post.model.js";
+import { uploadFile } from "../service/api.service.js";
+
+/**
+ * GET /api/users/search?q=abhi
+ */
+
+export const searchUser = async (req, res) => {
+  const { q } = req.query;
+  const currentUser = req.user.id;
+
+  const users = await userModel.aggregate(
+  [
+    {
+      $search: {
+        index: 'user_search_feature',
+        autocomplete: {
+          query: q,
+          path: 'username'
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: 'follows',
+        as: 'followdoc',
+        let: { searchUser: '$username' },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  {
+                    $eq: [
+                      '$followee',
+                      '$$searchUser'
+                    ]
+                  },
+                  {
+                    $eq: ['$follower', new mongoose.Types.ObjectId(currentUser)]
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        followStatus: {
+          $cond: {
+            if: {
+              $eq: [{ $size: '$followdoc' }, 0]
+            },
+            then: 'not following',
+            else: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $arrayElemAt: [
+                        '$followdoc.status',
+                        0
+                      ]
+                    },
+                    'pending'
+                  ]
+                },
+                then: 'requested',
+                else: 'following'
+              }
+            }
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        username: 1,
+        fullname: 1,
+        profilePicture: 1,
+        followStatus: 1
+      }
+    }
+  ],
+  { maxTimeMS: 60000, allowDiskUse: true }
+);
+
+  res.status(200).json({
+    message: "Users fetched successfully",
+    users,
+  });
+};
+
+export const followUser = async (req, res) => {
+  const { userId } = req.params;
+  const { id } = req.user;
+  console.log(id);
+  console.log("userId", userId);
+
+  const isUserExist = await userModel.findById(userId);
+
+  if (!isUserExist) {
+    return res.status(404).json({
+      message: "User not found",
+      success: false,
+    });
+  }
+
+  if (userId === id) {
+    return res.status(400).json({
+      message: "You cannot follow yourself",
+      success: false,
+    });
+  }
+
+  const alreadyFollowing = await followModel.findOne({
+    follower: id,
+    followee: userId,
+  });
+
+  if (alreadyFollowing) {
+    return res.status(400).json({
+      message: "You are already following this user",
+      success: false,
+    });
+  }
+
+  const follow = await followModel.create({
+    follower: id,
+    followee: userId,
+  });
+
+  return res.status(200).json({
+    message: "Follow request sent successfully",
+    success: true,
+    follow,
+  });
+};
+
+export const getFollowRequests = async (req, res) => {
+  const loggedInUserId = req.user.id;
+
+  const requests = await followModel.find({
+    followee: loggedInUserId,
+    status: "pending",
+  }).populate("follower","usernameprofilePicture");
+
+  return res.status(200).json({
+    message:"Follow requests fetched successfully",
+    success:true,
+    requests
+  })
+};
+
+
+
+
+export const acceptFollowRequest = async (req, res) => {
+
+    const { requestId } = req.params;
+    const loggedInUserId = req.user.id
+
+    const followRequest = await followModel.findOne({
+        _id: requestId,
+        status: "pending",
+        followee: loggedInUserId
+    })
+
+    if (!followRequest) {
+        return res.status(404).json({
+            message: "Follow request not found",
+            success: false,
+        })
+    }
+
+    followRequest.status = "accepted"
+    await followRequest.save()
+
+    return res.status(200).json({
+        message: "Follow request accepted successfully",
+        success: true,
+    })
+
+}
+
+export const getprofileData = async(req,res)=>{
+    const loggedInUserId = req.user.id
+    const followerCount = await followModel.countDocuments({
+        followee:loggedInUserId,
+        status:"accepted"
+    })
+
+    const followingCount = await followModel.countDocuments({
+        follower:loggedInUserId,
+        status:"accepted"
+    })
+
+    const followers = await followModel.find({
+        followee: loggedInUserId,
+        status: "accepted"
+    }).populate("follower", "username profilePicture fullname").lean()
+
+    const following = await followModel.find({
+        follower: loggedInUserId,
+        status: "accepted"
+    }).populate("followee", "username profilePicture fullname").lean()
+    
+    const posts = await postModel.find({
+        author: loggedInUserId,
+    }).lean()
+
+    res.status(200).json({
+        message:"Profile fetched successfully",
+        success:true,
+        profile:{
+            posts,
+            followerCount,
+            followingCount,
+            followers,
+            following,
+            postsCount : posts.length
+        }
+    })
+}
+
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { fullname, username, email } = req.body;
+    const file = req.file;
+
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    if (username && username !== user.username) {
+      const existingUsername = await userModel.findOne({ username });
+      if (existingUsername) {
+        return res.status(409).json({
+          message: "Username already taken",
+          success: false,
+        });
+      }
+      user.username = username;
+    }
+
+    if (email && email !== user.email) {
+      const existingEmail = await userModel.findOne({ email });
+      if (existingEmail) {
+        return res.status(409).json({
+          message: "Email already registered",
+          success: false,
+        });
+      }
+      user.email = email;
+    }
+
+    if (fullname) {
+      user.fullname = fullname;
+    }
+
+    if (file) {
+      const result = await uploadFile({
+        buffer: file.buffer,
+        fileName: file.originalname,
+      });
+      user.profilePicture = result.url;
+    }
+
+    await user.save();
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      success: true,
+      user: {
+        id: user.id,
+        email: user.email,
+        fullname: user.fullname,
+        username: user.username,
+        profilePicture: user.profilePicture,
+      },
+    });
+  } catch (error) {
+    console.error("Update Profile Error:", error);
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+}
---
+++ Backend/Docker/Day3-Dkr-20April/Class/Backend/src/sockets/app.socket.js
@@ -0,0 +1,65 @@
+import { parse } from "cookie"
+import { Server } from "socket.io"
+import { JWT_SECRET } from "../config/config.js"
+import jwt from 'jsonwebtoken'
+import chatMessageModel from "../models/Chat.model.js"
+
+
+
+export default function (httpServer){
+
+    const io = new Server(httpServer,{
+        cors: {
+            origin: "http://localhost:5173",
+            methods: [ "GET", "POST", "PUT", "DELETE", "PATCH" ],
+            credentials: true,
+        }
+    })
+    
+    io.use((socket, next)=>{
+        const cookie = socket.handshake.headers.cookie
+
+        if(!cookie){
+            return next(new Error("Authentication error: No token provided"))
+        }
+
+        const token = parse(cookie).token
+
+        try{
+            const decoded = jwt.verify(token,JWT_SECRET)
+            socket.user = decoded
+            next()
+
+        }catch(err){
+            next(err)
+        }
+    })
+    
+    io.on('connection', (socket) => {
+        console.log('A user connected:', socket.id);
+        console.log(socket.user)
+
+        socket.join(socket.user.id)
+
+        socket.on("send_message",async  data => {
+            const { message, receiver } = data
+            io.to(receiver).emit("receive_message", {
+                message,
+                sender: socket.user.id,
+            })
+
+             await chatMessageModel.create({
+                 sender:socket.user.id,
+                 receiver,
+                 message
+            })
+
+
+        })
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+            socket.leave(socket.user.id)
+        });
+    });
+}
